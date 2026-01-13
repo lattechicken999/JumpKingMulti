7a07b30a310e6fa26b97bb320c4d3827c4fb0317
lattechicken999 <whdtn218@gmail.com>
2026-01-13 23:01:27 +0900

    [Init] 초기 유니티 세팅 + Player Command 패턴 Command 업데이트

---
+++ Assets/Scripts/Player/PlayerCMDPattern/PlayerInvoker.cs
@@ -0,0 +1,72 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+public class PlayerInvoker : MonoBehaviourPun
+{
+    [SerializeField] float _increaseGageValue = 0.2f;
+    [SerializeField] float _minimumGageValue = 0.8f;
+    [SerializeField] float _maximumGageValue = 5f;
+    [SerializeField] float _moveSpeed = 3f;
+
+    private Rigidbody2D _rig;
+    private Animator _ani;
+
+    private bool _isPressJumpkey;
+    private float _jumpGage;
+    private Vector2 _dir;
+
+    private void Start()
+    {
+        _ani = GetComponent<Animator>();
+        _rig = GetComponent<Rigidbody2D>();
+
+        _dir = Vector2.zero; 
+        _isPressJumpkey = false;
+        _jumpGage = 0f;
+    }
+
+    public void SetMoveDir(Vector2 dir)
+    {
+        _dir = dir;
+    }
+    public void JumpStart()
+    {
+        _jumpGage = _minimumGageValue;
+        _isPressJumpkey = true;
+    }
+    public void JumpEnd()
+    {
+        if (!_isPressJumpkey) return;
+        _isPressJumpkey = false;
+
+        _rig.AddForce((_dir + Vector2.up).normalized * _jumpGage, ForceMode2D.Impulse);
+    }
+
+    private void PlayerMove()
+    {
+        if (_dir != Vector2.zero)
+            _rig.MovePosition(_dir * Time.deltaTime * _moveSpeed);  
+    }
+
+    private void Update()
+    {
+        if (_isPressJumpkey)
+        {
+            _jumpGage += _increaseGageValue * Time.deltaTime;
+
+            //점프게이지 가득차면 점프 실행
+            if(_jumpGage > _maximumGageValue)
+            {
+                _jumpGage = _maximumGageValue;
+                JumpEnd();
+            }
+        }
+        else
+        {
+            PlayerMove();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerCMDPattern/PlayerMotionCMD.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public interface IPlayerCommand
+{
+    void Execute();
+    IEnumerator Replay();
+}
+
+public class MoveCommand : IPlayerCommand
+{
+    Vector2 _dir;
+    PlayerInvoker _player;
+    public MoveCommand(PlayerInvoker _player, Vector2 dir)
+    {
+        this._dir = dir;
+        this._player = _player;
+    }
+    public void Execute()
+    {
+        _player.SetMoveDir(_dir);
+    }
+
+    public IEnumerator Replay()
+    {
+        Execute();
+        yield return null;
+    }
+}
+public class JumpCommand : IPlayerCommand
+{
+    bool _isJump;
+    PlayerInvoker _player;
+
+    public JumpCommand(PlayerInvoker player, bool isJump)
+    {
+        this._player = player;
+        this._isJump = isJump;
+    }
+    public void Execute()
+    {
+        if(_isJump)
+        {
+            _player.JumpStart();
+        }
+        else
+        {
+            _player.JumpEnd();
+        }
+    }
+
+    public IEnumerator Replay()
+    {
+        Execute();
+        yield return null;
+    }
+}
+public class WaitCommand : IPlayerCommand
+{
+    float _waitTime;
+    WaitForSeconds _waitRetern;
+    public WaitCommand(float time)
+    {
+        _waitTime = time;
+        _waitRetern = new WaitForSeconds(_waitTime);
+    }
+    public void Execute()
+    {
+
+    }
+    public IEnumerator Replay()
+    {
+        yield return _waitRetern;
+    }
+}
+
