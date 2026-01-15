ee80dbc2c7a830ebc33c8d02256c894d4b1f42d6
lattechicken999 <whdtn218@gmail.com>
2026-01-15 18:00:48 +0900

    [Feat] 회원 등록 기능 추가

--- Assets/Scripts/Login/FirebaseAuthManager.cs
+++ Assets/Scripts/Login/FirebaseAuthManager.cs
@@ -1,11 +1,12 @@
-﻿using UnityEngine;
-using Firebase;
+﻿using Firebase;
 using Firebase.Auth;
 using Firebase.Database;
-using System.Threading.Tasks;
-using UnityEngine.UI;
+using Photon.Pun.Demo.Cockpit;
 using System.Collections;
+using System.Threading.Tasks;
 using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 public class FirebaseAuthManager : MonoBehaviour
 {
     //firebase 관련 static 변수
@@ -102,5 +103,71 @@ public class FirebaseAuthManager : MonoBehaviour
             _user = loginTask.Result.User;
         }
     }
+    public void OnResistor()
+    {
+        StartCoroutine(ResisterCoroutine(_userEmailInpuField.text, _userPasswordField.text));
+    }
+    IEnumerator ResisterCoroutine(string email, string password)
+    {
+        Task<AuthResult> resistTask = _auth.CreateUserWithEmailAndPasswordAsync (email, password);
+        //로그인 완료 까지 대기
+        yield return new WaitUntil(predicate: () => resistTask.IsCompleted);
 
+        if (resistTask.Exception != null)
+        {
+            //로그인 실패 처리
+
+            Debug.LogWarning($"회원 가입 실패 : {resistTask.Exception}");
+
+            //firebase 에서 제공해주는 error exception으로 변경
+            FirebaseException firebaseEx = resistTask.Exception.GetBaseException() as FirebaseException;
+
+            AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
+
+            string message = "";
+
+            switch (errorCode)
+            {
+                case AuthError.MissingEmail:
+                    message = "Missing Email";
+                    break;
+                case AuthError.MissingPassword:
+                    message = "Missing Password";
+                    break;
+                case AuthError.WeakPassword:
+                    message = "Weak Password";
+                    break;
+                case AuthError.EmailAlreadyInUse:
+                    message = "Email Already In Use";
+                    break;
+                default:
+                    message = "Please contact us. whdtn218@gmail.com";
+                    break;
+            }
+            CallAlertUI(message);
+        }
+        else
+        {
+            _user = resistTask.Result.User;
+            if (_user != null)
+            {
+                UserProfile profile = new UserProfile { DisplayName = _userNickname.text };
+                Task profileTask = _user.UpdateUserProfileAsync(profile);
+                yield return new WaitUntil(predicate: () => profileTask.IsCompleted);
+
+                if (profileTask.Exception != null)
+                {
+                    Debug.LogWarning($"닉네임 설정 실패 : {profileTask.Exception}");
+                    FirebaseException firebaseEx = profileTask.Exception.GetBaseException() as FirebaseException;
+                    AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
+
+                    CallAlertUI( "닉네임 설정에 실패하였습니다.");
+                }
+                else
+                {
+                    CallAlertUI( "생성 완료, 반갑습니다." + _user.DisplayName + "님");
+                }
+            }
+        }
+    }
 }
