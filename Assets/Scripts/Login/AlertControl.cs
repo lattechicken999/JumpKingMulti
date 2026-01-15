8a90d8e8d709ce3a32ad8ac88e751b3e5241f4fa
lattechicken999 <whdtn218@gmail.com>
2026-01-15 17:40:26 +0900

    [Feat] 로그인 버튼 동작 완료

---
+++ Assets/Scripts/Login/AlertControl.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AlertControl : MonoBehaviour
+{
+    [Header("parameter")]
+    [SerializeField] float _viewDelay = 0.1f;
+    [SerializeField] float _dAlpha = 0.1f;
+    [SerializeField] float _alertDisplayTime = 1.5f;
+    [SerializeField] float _blank = 20f;
+
+    [Header("Connect Component")]
+    [SerializeField] Image _panelImg;
+    [SerializeField] TextMeshProUGUI _messageText;
+    [SerializeField] RectTransform _PannelTransform;
+    [SerializeField] RectTransform _TextTransform;
+
+
+    private Color _panelColor, _TextColor;
+    private WaitForSeconds _coroutineDelay;
+
+    private Coroutine _alertStartCoroutine, _alertCloseCoroutine;
+    private void Awake()
+    {
+        _coroutineDelay = new WaitForSeconds(_viewDelay);
+        gameObject.SetActive(false);
+    }
+    private void OnEnable()
+    {
+        UpdateAlpha(0);
+    }
+    private void OnDestroy()
+    {
+        if(_alertStartCoroutine != null)
+            StopCoroutine(_alertStartCoroutine);
+        if(_alertCloseCoroutine != null)
+            StopCoroutine(_alertCloseCoroutine);
+    }
+    private void UpdateAlpha(float alpha)
+    {
+        _panelColor = _panelImg.color;
+        _TextColor = _messageText.color;
+
+        _panelColor.a = alpha;
+        _TextColor.a = alpha;
+
+        _panelImg.color = _panelColor;
+        _messageText.color = _TextColor;
+    }
+    private void AlertStart()
+    {
+        gameObject.SetActive(true);
+        _alertStartCoroutine = StartCoroutine(AlertStartCoroutine());
+    }
+    private IEnumerator AlertStartCoroutine()
+    {
+        float alpha = 0f;
+        while (true)
+        {
+            alpha += _dAlpha;
+            UpdateAlpha(alpha);
+            if(alpha >= 1)
+            {
+                yield return new WaitForSeconds(_alertDisplayTime);
+                _alertCloseCoroutine = StartCoroutine(AlertCloseCoroutine());
+                yield break;
+            }
+            yield return _coroutineDelay;
+        }
+    }
+
+    private IEnumerator AlertCloseCoroutine()
+    {
+        float alpha = 1f;
+        while (true)
+        {
+            alpha -= _dAlpha;
+            UpdateAlpha(alpha);
+            if (alpha <= 0)
+            {
+                Destroy(gameObject);
+                yield break;
+            }
+            yield return _coroutineDelay;
+        }
+    }
+
+
+    public void UpdateTextMessage(string message)
+    {
+        _messageText.text = message;
+        var rectSize = _PannelTransform.sizeDelta;
+        rectSize.x = _messageText.preferredWidth + _blank;
+        _PannelTransform.sizeDelta = rectSize;
+
+        rectSize = _TextTransform.sizeDelta;
+        rectSize.x = _messageText.preferredWidth;
+        _TextTransform.sizeDelta = rectSize;
+
+        AlertStart();
+    }
+
+}
---
+++ Assets/Scripts/Login/FirebaseAuthManager.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
+using System.Threading.Tasks;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+public class FirebaseAuthManager : MonoBehaviour
+{
+    //firebase 관련 static 변수
+    private static FirebaseAuth _auth;
+    public static FirebaseAuth Auth => _auth;
+
+    private static FirebaseUser _user;
+    public static FirebaseUser User => _user;
+
+    private static DatabaseReference _dbRef;
+    public static DatabaseReference Database => _dbRef;
+
+
+    [Header("connect component")]
+    [SerializeField] TMP_InputField _userEmailInpuField;
+    [SerializeField] TMP_InputField _userPasswordField;
+    [SerializeField] TMP_InputField _userNickname;
+
+    [Header("Alert Prefeb")]
+    [SerializeField] GameObject _AlertObjectPrefeb;
+    [SerializeField] GameObject _canvas;
+
+    private IEnumerator Start()
+    {
+        var task =  Firebase.FirebaseApp.CheckAndFixDependenciesAsync();
+        yield return new WaitUntil(()=>task.IsCompleted);
+
+        var dependencyStatus = task.Result;
+        if (dependencyStatus == Firebase.DependencyStatus.Available)
+        {
+            _auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+            _dbRef = FirebaseDatabase.DefaultInstance.RootReference;
+        }
+        else
+        {
+            Debug.LogError("Firebase authentication failed");
+        }
+    }
+
+    private void CallAlertUI(string message)
+    {
+        var alertObject = Instantiate(_AlertObjectPrefeb, _canvas.transform);
+        AlertControl alertControl = alertObject.GetComponent<AlertControl>();
+        alertControl.UpdateTextMessage(message);
+    }
+    public void OnLogin()
+    {
+        StartCoroutine(LoginCoroutine(_userEmailInpuField.text,_userPasswordField.text));
+    }
+    IEnumerator LoginCoroutine(string email, string password)
+    {
+        Task<AuthResult> loginTask = _auth.SignInWithEmailAndPasswordAsync(email, password);
+        //로그인 완료 까지 대기
+        yield return new WaitUntil(predicate: () => loginTask.IsCompleted);
+
+        if(loginTask.Exception != null)
+        {
+            //로그인 실패 처리
+
+            Debug.LogWarning($"로그인 실패 : {loginTask.Exception}");
+
+            //firebase 에서 제공해주는 error exception으로 변경
+            FirebaseException firebaseEx = loginTask.Exception.GetBaseException() as FirebaseException;
+
+            AuthError errorCode = (AuthError) firebaseEx.ErrorCode;
+
+            string message = "";
+
+            switch (errorCode)
+            {
+                case AuthError.MissingEmail:
+                    message = "Missing Email";
+                    break;
+                case AuthError.MissingPassword:
+                    message = "Missing Password";
+                    break;
+                case AuthError.WrongPassword:
+                    message = "Wrong Password";
+                    break;
+                case AuthError.InvalidEmail:
+                    message = "Invalid Email";
+                    break;
+                case AuthError.UserNotFound:
+                    message = "Not Found Email";
+                    break;
+                default:
+                    message = "Please contact us. whdtn218@gmail.com";
+                    break;
+            }
+            CallAlertUI(message);
+        }
+        else
+        {
+            _user = loginTask.Result.User;
+        }
+    }
+
+}
